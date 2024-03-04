34442c2659590fd1839360dbbf20752673937f87
myungji <myungju030@gmail.com>
2024-03-04 12:05:18 +0900

    fix : 오디오 api 연동

--- src/services/tour.js
+++ src/services/tour.js
@@ -8,3 +8,7 @@ export const getTourInfoAPI = () => {
 export const getTourPlaceInfoAPI = (id) => {
   return apiRequest.get(`${API_URLS.tour}/${id}`);
 };
+
+export const getTourAudioInfoAPI = (id) => {
+  return apiRequest.get(`${API_URLS.tour}/${id}/speak`);
+};
